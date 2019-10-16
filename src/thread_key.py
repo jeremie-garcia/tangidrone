4442d24504a693b24986ffee18209ed84c63d701
lucas <li>
2019-10-16 17:22:12 +0200

    ajout des nouvelles fonc + détection des config

---
+++ src/thread_key.py
@@ -0,0 +1,30 @@
+from threading import Thread,Lock
+import time
+import keyboard
+
+class globalVars():
+    pass
+
+G = globalVars() #empty object to pass around global state
+G.lock = Lock() #not really necessary in this case, but useful none the less
+G.key = None
+G.kill = False
+
+def foo(n): #function doing intense computation
+    while True:
+        if G.kill:
+            G.kill = False
+            return
+        with G.lock:
+            print(G.key)
+
+t = Thread(target=foo, args=(10,))
+t.start()
+
+def askinput():
+    #change to raw_input for python 2.7
+    G.key = keyboard.read_key()
+    return 1
+
+while askinput():
+    pass
